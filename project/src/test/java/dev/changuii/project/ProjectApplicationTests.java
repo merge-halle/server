ec5de8aedbcce5a45174eefb83f38dd0c20b61b4
changuii <rhljh7410@gmail.com>
2024-08-13 16:13:00 +0900

    테스트 주석처리

--- project/src/test/java/dev/changuii/project/ProjectApplicationTests.java
+++ project/src/test/java/dev/changuii/project/ProjectApplicationTests.java
@@ -1,13 +1,13 @@
-package dev.changuii.project;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package dev.changuii.project;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProjectApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
