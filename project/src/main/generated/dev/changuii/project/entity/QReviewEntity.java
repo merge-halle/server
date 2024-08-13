b9efba0aedba1f5c8d6a1991b6ccccd1c9b8c810
changuii <rhljh7410@gmail.com>
2024-08-14 04:36:10 +0900

    질문 수정

---
+++ project/src/main/generated/dev/changuii/project/entity/QReviewEntity.java
@@ -0,0 +1,39 @@
+package dev.changuii.project.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QReviewEntity is a Querydsl query type for ReviewEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReviewEntity extends EntityPathBase<ReviewEntity> {
+
+    private static final long serialVersionUID = 1752022396L;
+
+    public static final QReviewEntity reviewEntity = new QReviewEntity("reviewEntity");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath review = createString("review");
+
+    public QReviewEntity(String variable) {
+        super(ReviewEntity.class, forVariable(variable));
+    }
+
+    public QReviewEntity(Path<? extends ReviewEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QReviewEntity(PathMetadata metadata) {
+        super(ReviewEntity.class, metadata);
+    }
+
+}
+
